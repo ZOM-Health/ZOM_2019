61929574c29b9b272e9fd6fc885ad9c1b550a3a4
Soleil Walker <35232955+SoleilWalker@users.noreply.github.com>
2019-08-25 01:34:48 +0300

    Designated Staking files uploaded

--- truffle-config.js
+++ truffle-config.js
@@ -3,18 +3,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
-      gas: 5000000
-    }
-  },
-  compilers: {
-    solc: {
-      settings: {
-        optimizer: {
-          enabled: true, // Default: false
-          runs: 200      // Default: 200
-        },
-      }
-    }
+      network_id: "*" // Match any network id
+    },
   }
 };
